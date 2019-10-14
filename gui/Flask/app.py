2add6a3214d5dc95b619b9832d99978050a2d50a
Flavio <fflavioar@gmail.com>
2019-10-14 13:43:53 -0300

    Refactoring

--- gui/Flask/app.py
+++ gui/Flask/app.py
@@ -1,6 +1,3 @@
-#import StandardPCRPrimer as standard
-#import MultigenePCRPrimer as multigene
-
 from wrapper import Multigene
 from flask import Flask
 from flask import render_template, request
@@ -34,12 +31,10 @@ def search():
                  form.max_content.data, 
                  form.min_tm.data, 
                  form.max_tm.data, 
-                 16, 
-                 4, 
-                 14, 
-                 4, 
                  form.min_product_length.data,
-                 form.max_product_length.data]
+                 form.max_product_length.data,
+                 form.primer_concentration.data,
+                 form.strand_concentration.data]
 
         multigene = Multigene()
         sequences = get_sequences(form.sequence.data)
@@ -52,19 +47,19 @@ def search():
 
 
 class ParameterForm(Form):
-    sequence = TextAreaField('Sequences (5\'&rarr;3\'): ', [validators.length(max = 10000), validators.Required("Sequence is required.")])
+    sequence = TextAreaField('Sequences (5\'&rarr;3\'): ', [validators.length(max = 10000), validators.Required("Sequences are required.")])
 
-    min_length = IntegerField("Min.", [validators.Required("Min. length is required.")], default=18)
-    max_length = IntegerField("Max.", [validators.Required("Max. length is required.")], default=24)
+    min_length = IntegerField("Min.", [validators.Required("Minimum length is required.")], default=18)
+    max_length = IntegerField("Max.", [validators.Required("Maximum length is required.")], default=24)
 
-    min_content = IntegerField("Min.", [validators.Required("Min. GC content is required.")], default=40)
-    max_content = IntegerField("Max.", [validators.Required("Max. GC content is required.")], default=60)
+    min_content = IntegerField("Min.", [validators.Required("Minimum GC content is required.")], default=40)
+    max_content = IntegerField("Max.", [validators.Required("Maximum GC content is required.")], default=60)
 
-    min_tm = IntegerField("Min.", [validators.Required("Min. melting temperature is required.")], default=50)
-    max_tm = IntegerField("Max.", [validators.Required("Max. melting temperature is required.")], default=65)
+    min_tm = IntegerField("Min.", [validators.Required("Minimum melting temperature is required.")], default=50)
+    max_tm = IntegerField("Max.", [validators.Required("Maximum melting temperature is required.")], default=65)
 
-    min_product_length = IntegerField('Min.', [validators.Required("Min. product length is required.")], default=100)
-    max_product_length = IntegerField('Max.', [validators.Required("Max. product length is required.")], default=1200)
+    min_product_length = IntegerField('Min.', [validators.Required("Minimum product length is required.")], default=100)
+    max_product_length = IntegerField('Max.', [validators.Required("Maximum product length is required.")], default=1200)
 
-    concentration_primer = DecimalField('Primer', [validators.Required("Primer concentration is required.")], places=6, default=0.000001)
-    concentration_strand = DecimalField('Strand', [validators.Required("Strand concentration is required.")], places=6, default=0.000001)
+    primer_concentration = DecimalField('Primer', [validators.Required("Primer concentration is required.")], places=6, default=0.000001)
+    strand_concentration = DecimalField('Strand', [validators.Required("Strand concentration is required.")], places=6, default=0.000001)
