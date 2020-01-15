038a24dd7909ec369fc3031f7156683f1f6a24fc
Flavio <fflavioar@gmail.com>
2020-01-15 16:03:55 -0300

    Implementing struct to data input

--- src/multigene.h
+++ src/multigene.h
@@ -3,7 +3,7 @@
 
 struct TPrimer 
 {
-    int target_id;				// Target sequence identification
+    char *target_id;			// Target sequence identification
     char *sequence;				// Primer sequence
     int length;					// Primer length
     int position;				// Begin position in target
@@ -11,7 +11,7 @@ struct TPrimer
     bool reverse;				// Indicate whether primer is reverse
     float gc_content;			// GC content
     float temperature;			// Melting temperature in C
-    float free_energy;			// Free energy change (dG) of hairpin formation in cal/mol 
+    // float free_energy;		// Free energy change (dG) of hairpin formation in cal/mol 
     // int number;				// Number of non target sequences
     float *forward_elongation_efficiency; // Forward elongation efficiency
     float *reverse_elongation_efficiency; // Reverse elongation efficiency
@@ -21,15 +21,23 @@ typedef TPrimer Primer;
 
 struct TPair 
 {
-    int target_id;				// Target identification
+    char *target_id;			// Target identification
     int product_size;			// PCR Product size 
-    float free_energy;			// Free energy change (dG) of dimer formation in cal/mol
+    // float free_energy;		// Free energy change (dG) of dimer formation in cal/mol
     Primer *forward;			// Forward primer
     Primer *reverse;			// Reverse primer
     struct TPair *next;
 };
 typedef TPair Pair;
 
+struct TTarget
+	{
+		char *id;
+		char *sequence;
+	};
+	typedef TTarget Target;
+
 // extern "C" Pair *design(char **targets, int m);
-Pair *design(char **targets, int m);
+// Pair *design(char **targets, int m);
+Pair *design(Target *targets, int m);
 #endif
